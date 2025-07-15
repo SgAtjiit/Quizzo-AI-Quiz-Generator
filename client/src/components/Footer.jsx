6cd773f30ccf14deff3093d960d2d9f365d57a87
Shrish <shrishpankajguptadbd6@gmail.com>
2025-07-15 18:44:30 +0530

    updated footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
         </p>
         <div className="flex space-x-4 mt-2 sm:mt-0">
           <a
-            href="https://drive.google.com/file/d/1hbHqnvqcnK72u4pNocDAsqloPR2bfgq4/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1Z8HHjv4X613gLAZ5gO_sCU0B1emsQ3QI/view?usp=sharing"
             target="_blank"
             className="hover:text-blue-400"
           >
